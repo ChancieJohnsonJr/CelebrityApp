45e1f774c3f31cb7f0efee6bd7cbc750fabc58cb
Chancie Johnson <chancie.johnson@enhanceit.us>
2020-03-16 03:10:20 -0400

    update

--- app/src/main/java/com/example/celebrityapp/MarkFavorite.kt
+++ app/src/main/java/com/example/celebrityapp/MarkFavorite.kt
@@ -3,7 +3,13 @@ package com.example.celebrityapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import kotlinx.android.synthetic.main.activity_my_favorites*
+import kotlinx.android.synthetic.main.activity_mark_favorite
+import kotlinx.android.synthetic.main.celebrity_item.*
+
+*
+
+
+
 
 
 class MarkFavorite : AppCompatActivity() {
@@ -14,5 +20,22 @@ class MarkFavorite : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mark_favorite)
+    }
+
+    fun onClick(view: View) {
+        when(view.id){
+            R.id.btnGetCelebInfo -> {
+                val celebId = etCelebID.text.toString()
+                databaseHelper.getOneCelebFromDatabase(celebId)
+            }
+
+            R.id.btnMakeFaveCeleb -> {
+                val firstName = tvFirstName.text.toString()
+                val lastName = tvLastName.text.toString()
+                val celebId = tvCelebID.text.toString()
+                val isFavorite = tvIsFavorite.text.toString()
+
+            }
+        }
     }
 }
